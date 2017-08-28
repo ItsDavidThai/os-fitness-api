94454336f5954ecc9ab0ae2c4849309729a3d3dc
itsdavidthai <itsdavidthai@gmail.com>
2017-08-28 00:46:09 -0600

    change server.js

--- server.js
+++ server.js
@@ -6,22 +6,23 @@ app.set("port", process.env.PORT || 8080);
 app.set("api-port", process.env.APIPORT || 3005);
 
 // Express only serves static assets in production
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-}
-app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("client/build"));
+// }
+// app.use(express.static("client/build"));
 
 
 app.listen(app.get("port"), () => {
 console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
 });
 
+//
+// app.listen(app.get("api-port"), () => {
+// console.log(`Find the server at: http://localhost:${app.get("api-port")}/`); // eslint-disable-line no-console
+// });
 
-app.listen(app.get("api-port"), () => {
-console.log(`Find the server at: http://localhost:${app.get("api-port")}/`); // eslint-disable-line no-console
-});
 
-
-app.get('/exercises', (req, res) => {
+app.get('/', (req, res) => {
     res.json('hello')
 })
+
